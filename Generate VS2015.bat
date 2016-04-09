62fc40bf3e0250fab6d72884ca91f7633fe7d80d
Henry Read <henrya2@126.com>
2016-04-09 22:13:45 +0800

    Fixes wrong data path

--- Generate VS2015.bat
+++ Generate VS2015.bat
@@ -7,7 +7,7 @@ xcopy "%~dp0\OpenSceneGraph\3rdParty\bin\*.*" "%~dp0\build\OpenSceneGraph\OpenSc
 cd build
 cmake -G "Visual Studio 14 2015 Win64" ..
 
-SET OSG_FILE_PATH="%~dp0OpenSceneGraph-Data\"
+SET OSG_FILE_PATH=%~dp0OpenSceneGraph-Data
 
 OpenSceneGraphSamples.sln
 
